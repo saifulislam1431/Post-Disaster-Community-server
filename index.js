3503ef5904490caec1757ea445840e14f52130ed
Saiful Islam <sakibbhaiya143@gmail.com>
2024-02-19 11:43:29 +0600

    Finished

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://post-disaster-supply-chain.web.app', credentials: true }));
 app.use(express.json());
 
 // MongoDB Connection URL
